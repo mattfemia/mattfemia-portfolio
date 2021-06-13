61b2e08bac9e64ac6a4877cc1c03e9cfc9392c76
Matt Femia <mattfemia1@gmail.com>
2021-06-13 19:11:52 -0400

    Remove test_ContactForm from tests/functional/test_main.py

--- tests/functional/test_main.py
+++ tests/functional/test_main.py
@@ -16,21 +16,3 @@ def test_home_page_post(test_client):
     """
     response = test_client.post('/')
     assert response.status_code == 200
-
-def test_ContactForm(test_client):
-    """
-    GIVEN ContactForm form model
-    CHECK params 
-    """
-
-    data = {
-        'name': "Matt Femia", 
-        'email': "mattfemia1@gmail.com", 
-        'message': "Hello"
-    }
-
-    response = test_client.post('/', data=json.dumps(data),
-        follow_redirects=True
-    )
-    assert response.status_code == 200
-    assert b"Message sent!" in response.data
